b8de31966190b02ea745a16a923f15106fb553a3
thdmsdl97 <thdmsdl97@gmail.com>
2022-10-03 21:57:46 +0900

    CodeUp 1001~1019

---
+++ basic/codeUp1018.java
@@ -0,0 +1,12 @@
+package basic;
+
+import java.util.Scanner;
+
+public class codeUp1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+
+        System.out.printf(a);
+    }
+}
